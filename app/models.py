d4123427fa566697b3be0c1c4e6e59b8d5ec6f76
KuthikBohdan1 <bkuzik14@gmail.com>
2025-05-18 23:09:28 +0300

    1

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+def teacher_models(models.Model):
+
+    Teacher = models.CharField(max_length=100)
+    Subject = models.CharField(max_length=100)
+
+def student_models(models.Model):
+
+    Student = models.CharField(max_length=100)
+    Class =  models.CharField(max_length=100)
