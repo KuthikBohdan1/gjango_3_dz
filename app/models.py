a81cfb16a8b09e74fedf939903dd208be1faf42e
KuthikBohdan1 <bkuzik14@gmail.com>
2025-05-24 12:28:11 +0300

    r

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Subject(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Ім'я")
-    description = models.CharField(max_length=100, verbose_name="опис")
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
@@ -12,20 +12,22 @@ class Teacher(models.Model):
     name = models.CharField(max_length=100)
     last_name  = models.CharField(max_length=100)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
 
-class Class (models.Model):
+class Class(models.Model):
     name = models.CharField(max_length=100)
     year = models.IntegerField(default=1)
 
+    def __str__(self):
+        return self.name
+    
 class Student(models.Model):
     name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    student_class = models.CharField(Class , on_delete=models.CASCADE)
-
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    student_class = models.ForeignKey(Class, on_delete=models.CASCADE)
     def __str__(self):
-        return self.student
-
-
+        return self.name
 
 class Schedule(models.Model):
     DAYS = (
@@ -45,16 +47,12 @@ class Schedule(models.Model):
     Teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
-
-##
-
+        return f'день {self.date}, {self.start_time} ,придмет {self.subject}'
 class Grade(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     Subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     grade = models.IntegerField()
     date = models.DateTimeField(auto_now_add = True)
-
 
 
 
--- main.py
+++ main.py
@@ -4,4 +4,60 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 django.setup()
-from app.models import User, Group
+from app.models import Class, Subject, Teacher, Student, Schedule, Grade    
+
+work = True
+while work == True:
+    print("""
+        1. додати придмет
+        2. додати вчителя
+        3. додати клас
+        4. додати учня
+        0. вийти
+
+
+""")
+    
+    chois = int(input("Введіть номер дії: "))
+    if chois == 0:
+        work = False
+
+    if chois == 1:
+        name = input("назва придмету: ")
+        description = input("опис: ")
+        check = Subject.objects.filter(name = name).first()
+        if check:
+            print("вже існує")
+
+        else:
+            Subject.objects.create(name=name, description=description)
+
+
+    if chois == 2:
+        name = input("імя: ")   
+        last_name = input("призвище: ")
+        subject = input("предмет: ")
+        check = subject.objects.filter(name = subject).first()
+        if check:
+            Teacher.objects.cereate(name = name, last_name = last_name, subject = check)
+
+        else:
+            print("такого придмету немає")
+
+
+        
+    if chois == 3:  
+        name = input("назва придмету: ")
+        year = int(input("опис: "))
+        check = Class.objects.filter(name = name).first()
+        if check:
+            print("вже існує")
+
+        else:
+            Class.objects.create(name=name, year= year)
+            print("клас додано")
+
+
+    if chois == 4:
+
+        pass
