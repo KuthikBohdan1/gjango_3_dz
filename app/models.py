434bc1e396774ede2a6fef207ebf6cc8fdd4555a
KuthikBohdan1 <bkuzik14@gmail.com>
2025-05-24 11:36:00 +0300

    43

--- app/models.py
+++ app/models.py
@@ -1,15 +1,29 @@
 from django.db import models
 
-class Teacher(models.Model):
-    teacher_name = models.CharField(max_length=100, verbose_name="Ім'я вчителя")
-    subject = models.CharField(max_length=100, verbose_name="Предмет")
+class Subject(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Ім'я")
+    description = models.CharField(max_length=100, verbose_name="опис")
 
     def __str__(self):
-        return self.teacher_name
+        return self.name
+    
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    last_name  = models.CharField(max_length=100)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
 
 class Student(models.Model):
-    student_name = models.CharField(max_length=100, verbose_name="Ім'я студента")
-    student_class = models.CharField(max_length=100, verbose_name="Клас")
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    student_class = models.CharField(Class , on_delete=models.CASCADE)
 
     def __str__(self):
         return self.student_name
+
+class Class (models.Model):
+    name = models.CharField(max_length=100)
+    year = models.IntegerField(default=1)
+
+class Schedule(models.Model):
+    clas = models.FreignKey(subject, on_delete=models.CASCADE)
